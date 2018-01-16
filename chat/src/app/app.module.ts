465438fa5297db5e2d57cc286fec5def2996f8fe
Aaron Balthaser <aaron@aarons-mbp.attlocal.net>
2018-01-16 04:18:50 -0800

    first commit - project setup.

---
+++ chat/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+/* Module Dependencies */
+
+/* Containers */
+import { AppComponent } from './app.component';
+
+/* Components */
+
+/* Services */
+
+@NgModule({
+  imports: [
+    BrowserModule
+  ],
+  declarations: [
+    AppComponent
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+
+export class AppModule {}
